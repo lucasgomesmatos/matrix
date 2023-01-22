69f85625fe153bfff7afce8b752e46bd307f5dfe
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-01-22 16:53:18 -0300

    video

--- src/components/videoPlayer/index.tsx
+++ src/components/videoPlayer/index.tsx
@@ -6,11 +6,13 @@ export const VideoPlayer = ({ link }: IVideoPlayer) => {
   return (
     <div>
       <iframe
-        width="980"
-        height="550"
-        src={`https://www.youtube.com/embed/${link}?enablejsapi=0&amp;autoplay=0&amp;modestbranding=1&amp;controls=0&amp;showinfo=0&amp;rel=0&amp;hd=1&amp;wmode=transparent&amp;enablejsapi=0`}
+        width="960"
+        height="560"
+        src={`https://www.youtube-nocookie.com/embed/${link}`}
+        title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
       ></iframe>
     </div>
   );
